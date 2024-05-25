a5fc5650c5a6d0d81e8618183a9a7208be6d5ee8
chuanh1214 <chuthianh2202chuthianh22@gmail.com>
2024-05-25 18:54:01 +0700

    cap nhat lai giao dien quan ly thu nhap va quan ly chi tieu

--- pages/main/quanlychitieu.php
+++ pages/main/quanlychitieu.php
@@ -5,14 +5,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Money Manager</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
-        integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link href="assets/bootstrap/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="assets/css/base.css">
-    <link rel="stylesheet" href="assets/css/qlchitieu.css">
-    <link rel="stylesheet" href="assets/css/themsuaxoaQLCT.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="../../assets/bootstrap/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="../../assets/css/base.css">
+    <link rel="stylesheet" href="../../assets/css/qlchitieu.css">
+    <link rel="stylesheet" href="../../assets/css/themsuaxoaQLCT.css">
 </head>
 
 <body>
@@ -23,14 +20,14 @@
             <div class="row">
                 <div class="col-12">
                     <div class="inner-main">
-                        <a class="inner-logo" href="./index.html">
-                            <img src="./assets/images/moneyManager.png" alt="Logo moneyManager">
+                        <a class="inner-logo" href="../../index.html">
+                            <img src="../../assets/images/moneyManager.png" alt="Logo moneyManager">
                             <span class="inner-name">Money Manager</span>
                         </a>
                         <div class="inner-menu">
                             <ul>
-                                <li><a href="./quanlithunhap.html">Thu Nhập</a></li>
-                                <li><a href="#">Chi Tiêu</a></li>
+                                <li><a href="quanlythunhap.php">Thu Nhập</a></li>
+                                <li><a href="quanlychitieu.php">Chi Tiêu</a></li>
                                 <li><a href="#">Tiết kiệm</a></li>
                                 <li><a href="#">Quản Lý Ngân Sách</a></li>
                                 <li><a href="#">Xem Đánh Dấu</a></li>
@@ -53,9 +50,9 @@
 
     <!-- BODY Quan ly chi tieu -->
     <div class="container">
-        <h1>Danh sách chi tiêu</h1>
-         <!-- THEM -->
-        <button  onclick="momodal()"><span>Thêm</span></button>
+        <h1 class="pt-5">Danh sách chi tiêu</h1>
+        <!-- THEM -->
+        <button onclick="momodal()"><span>Thêm</span></button>
         <div class="nenmodal" id="nenmodal-1">
             <div class="nenmodal2"></div>
             <div class="ndmodal">
@@ -66,32 +63,32 @@
                         <div class="col-12">
                             <div class="form">
                                 <form action="https://www.w3schools.com/action_page.php" target="_blank">
-                                    <div class ="form-child">
+                                    <div class="form-child">
                                         <label for="text">Tên khoản chi tiêu</label>
-                                        <input type="text" name="full_name" >
+                                        <input type="text" name="full_name">
                                     </div>
-                                    <div class ="form-child">
+                                    <div class="form-child">
                                         <label for="text">Số tiền</label>
-                                        <input type="tel" name="phone_number" >
+                                        <input type="tel" name="phone_number">
                                     </div>
-                                    <div class ="form-child">
+                                    <div class="form-child">
                                         <label for="text">Ghi chú</label>
-                                        <input type="email" name="email" >
+                                        <input type="email" name="email">
                                     </div>
-                                    <div class ="form-child">
+                                    <div class="form-child">
                                         <label for="text">Ngày</label>
-                                        <input type="password" name="password" >
+                                        <input type="password" name="password">
                                     </div>
-                                    <div class ="form-child">
+                                    <div class="form-child">
                                         <label for="text">Loại chi tiêu</label>
-                                        <input type="password" name="password" >
+                                        <input type="password" name="password">
                                     </div>
-                                    <div class ="form-child">
+                                    <div class="form-child">
                                         <button type="submit">Thêm</button>
                                     </div>
-    
+
                                 </form>
-                              </div>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -110,31 +107,31 @@
                         <div class="col-12">
                             <div class="form">
                                 <form action="https://www.w3schools.com/action_page.php" target="_blank">
-                                    <div class ="form-child">
+                                    <div class="form-child">
                                         <label for="text">Tên khoản chi tiêu</label>
-                                        <input type="text" name="full_name" >
+                                        <input type="text" name="full_name">
                                     </div>
-                                    <div class ="form-child">
+                                    <div class="form-child">
                                         <label for="text">Số tiền</label>
-                                        <input type="tel" name="phone_number" >
+                                        <input type="tel" name="phone_number">
                                     </div>
-                                    <div class ="form-child">
+                                    <div class="form-child">
                                         <label for="text">Ghi chú</label>
-                                        <input type="email" name="email" >
+                                        <input type="email" name="email">
                                     </div>
-                                    <div class ="form-child">
+                                    <div class="form-child">
                                         <label for="text">Ngày</label>
-                                        <input type="password" name="password" >
+                                        <input type="password" name="password">
                                     </div>
-                                    <div class ="form-child">
+                                    <div class="form-child">
                                         <label for="text">Loại chi tiêu</label>
-                                        <input type="password" name="password" >
+                                        <input type="password" name="password">
                                     </div>
-                                    <div class ="form-child">
+                                    <div class="form-child">
                                         <button type="submit">Cập Nhật</button>
                                     </div>
                                 </form>
-                              </div>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -156,15 +153,13 @@
 
         <!-- BANG CHI TIEU -->
         <table border="1">
-            <caption align="bottom">DANH SÁCH CHI TIÊU</caption>
             <tr>
                 <th>Tên khoản chi tiêu</th>
                 <th>Số tiền</th>
                 <th>Ghi chú</th>
                 <th>Ngày</th>
                 <th>Loại chi tiêu</th>
-                <th></th>
-                <th></th>
+                <th>Thao tác</th>
             </tr>
             <tr>
                 <td>Bánh gà</td>
@@ -172,8 +167,9 @@
                 <td></td>
                 <td>1/6/2023</td>
                 <td>Đồ ăn</td>
-                <td><button  onclick="momodal2()"><span>Sửa</span></button></td>
-                <td><button  onclick="momodal3()"><span>Xóa</span></button></td>
+                <td><a href="#" onclick="momodal2()" class="btn btn-success" name="suathunhap">Sửa</a>
+                    <a href="#" onclick="momodal3()" class="btn btn-danger" name="xoathunhap" onclick="confirm('Bạn có chắc chắn muốn xóa?')">Xóa</a>
+                </td>
             </tr>
             <tr>
                 <td>Quần áo bà ba</td>
@@ -181,8 +177,9 @@
                 <td>Tặng bà</td>
                 <td>1/6/2023</td>
                 <td>Quần áo</td>
-                <td><button  onclick="momodal2()"><span>Sửa</span></button></td>
-                <td><button  onclick="momodal3()"><span>Xóa</span></button></td>
+                <td><a href="#" onclick="momodal2()" class="btn btn-success" name="suathunhap">Sửa</a>
+                    <a href="#" onclick="momodal3()" class="btn btn-danger" name="xoathunhap" onclick="confirm('Bạn có chắc chắn muốn xóa?')">Xóa</a>
+                </td>
             </tr>
             <tr>
                 <td>Tiền điện tháng 5</td>
@@ -190,8 +187,9 @@
                 <td></td>
                 <td>7/6/2023</td>
                 <td>Tiền điện</td>
-                <td><button  onclick="momodal2()"><span>Sửa</span></button></td>
-                <td><button  onclick="momodal3()"><span>Xóa</span></button></td>
+                <td><a href="#" onclick="momodal2()" class="btn btn-success" name="suathunhap">Sửa</a>
+                    <a href="#" onclick="momodal3()" class="btn btn-danger" name="xoathunhap" onclick="confirm('Bạn có chắc chắn muốn xóa?')">Xóa</a>
+                </td>
             </tr>
             <tr>
                 <td>Mua trà sữa</td>
@@ -199,8 +197,9 @@
                 <td></td>
                 <td>7/6/2023</td>
                 <td>Đồ uống</td>
-                <td><button  onclick="momodal2()"><span>Sửa</span></button></td>
-                <td><button  onclick="momodal3()"><span>Xóa</span></button></td>
+                <td><a href="#" onclick="momodal2()" class="btn btn-success" name="suathunhap">Sửa</a>
+                    <a href="#" onclick="momodal3()" class="btn btn-danger" name="xoathunhap" onclick="confirm('Bạn có chắc chắn muốn xóa?')">Xóa</a>
+                </td>
             </tr>
             <tr>
                 <td>Đi du lịch</td>
@@ -208,8 +207,9 @@
                 <td>Hạ Long</td>
                 <td>8/6/2023</td>
                 <td>Du lịch</td>
-                <td><button  onclick="momodal2()"><span>Sửa</span></button></td>
-                <td><button  onclick="momodal3()"><span>Xóa</span></button></td>
+                <td><a href="#" onclick="momodal2()" class="btn btn-success" name="suathunhap">Sửa</a>
+                    <a href="#" onclick="momodal3()" class="btn btn-danger" name="xoathunhap" onclick="confirm('Bạn có chắc chắn muốn xóa?')">Xóa</a>
+                </td>
             </tr>
             <tr>
                 <td>Nhét lợn</td>
@@ -217,8 +217,9 @@
                 <td></td>
                 <td>8/6/2023</td>
                 <td>Tiết kiệm</td>
-                <td><button  onclick="momodal2()"><span>Sửa</span></button></td>
-                <td><button  onclick="momodal3()"><span>Xóa</span></button></td>
+                <td><a href="#" onclick="momodal2()" class="btn btn-success" name="suathunhap">Sửa</a>
+                    <a href="#" onclick="momodal3()" class="btn btn-danger" name="xoathunhap" onclick="confirm('Bạn có chắc chắn muốn xóa?')">Xóa</a>
+                </td>
             </tr>
         </table>
     </div>
@@ -244,17 +245,18 @@
             </div>
         </div>
     </footer>
-    <script src="assets/bootstrap/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+    <script src="../../assets/bootstrap/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
     </script>
     <script>
-        function momodal(){
+        function momodal() {
             document.getElementById("nenmodal-1").classList.toggle("active");
         }
-        function momodal2(){
+
+        function momodal2() {
             document.getElementById("nenmodal-2").classList.toggle("active");
         }
-        function momodal3(){
+
+        function momodal3() {
             document.getElementById("nenmodal-3").classList.toggle("active");
         }
     </script>
--- pages/main/quanlythunhap.php
+++ pages/main/quanlythunhap.php
@@ -5,14 +5,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Money Manager</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
-        integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link href="assets/bootstrap/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="assets/css/base.css">
-    <link rel="stylesheet" href="assets/css/qlchitieu.css">
-    <link rel="stylesheet" href="assets/css/themsuaxoaQLTN.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="../../assets/bootstrap/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="../../assets/css/base.css">
+    <link rel="stylesheet" href="../../assets/css/qlthunhap.css">
+    <link rel="stylesheet" href="../../assets/css/themsuaxoaQLTN.css">
 </head>
 
 <body>
@@ -24,13 +21,13 @@
                 <div class="col-12">
                     <div class="inner-main">
                         <a class="inner-logo" href="./index.html">
-                            <img src="./assets/images/moneyManager.png" alt="Logo moneyManager">
+                            <img src="../../assets/images/moneyManager.png" alt="Logo moneyManager">
                             <span class="inner-name">Money Manager</span>
                         </a>
                         <div class="inner-menu">
                             <ul>
-                                <li><a href="#">Thu Nhập</a></li>
-                                <li><a href="./quanlychitieu.html">Chi Tiêu</a></li>
+                                <li><a href="quanlythunhap.php">Thu Nhập</a></li>
+                                <li><a href="quanlychitieu.php">Chi Tiêu</a></li>
                                 <li><a href="#">Tiết kiệm</a></li>
                                 <li><a href="#">Quản Lý Ngân Sách</a></li>
                                 <li><a href="#">Xem Đánh Dấu</a></li>
@@ -53,9 +50,9 @@
 
     <!-- BODY Quan ly chi tieu -->
     <div class="container">
-        <h1>Danh sách thu nhập</h1>
-         <!-- THEM -->
-        <button  onclick="momodal()"><span>Thêm</span></button>
+        <h1 class="pt-5">Danh sách thu nhập</h1>
+        <!-- THEM -->
+        <button onclick="momodal()"><span>Thêm</span></button>
         <div class="nenmodal" id="nenmodal-1">
             <div class="nenmodal2"></div>
             <div class="ndmodal">
@@ -66,24 +63,24 @@
                         <div class="col-12">
                             <div class="form">
                                 <form action="https://www.w3schools.com/action_page.php" target="_blank">
-                                    <div class ="form-child">
+                                    <div class="form-child">
                                         <label for="text">Tên khoản thu nhập</label>
-                                        <input type="text" name="full_name" >
+                                        <input type="text" name="full_name">
                                     </div>
-                                    <div class ="form-child">
+                                    <div class="form-child">
                                         <label for="text">Số tiền</label>
-                                        <input type="tel" name="phone_number" >
+                                        <input type="tel" name="phone_number">
                                     </div>
-                                    <div class ="form-child">
+                                    <div class="form-child">
                                         <label for="text">Ghi chú</label>
-                                        <input type="email" name="email" >
+                                        <input type="email" name="email">
                                     </div>
-                                    <div class ="form-child">
+                                    <div class="form-child">
                                         <button type="submit">Thêm</button>
                                     </div>
-    
+
                                 </form>
-                              </div>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -96,30 +93,30 @@
             <div class="nenmodal2"></div>
             <div class="ndmodal">
                 <div class="closemodal"><button onclick="momodal2()">×</button></div>
-                <div class="titlemodal">Sửa khoản chi tiêu </div>
+                <div class="titlemodal">Sửa khoản thu nhập </div>
                 <div class="container1">
                     <div class="row">
                         <div class="col-12">
                             <div class="form">
                                 <form action="https://www.w3schools.com/action_page.php" target="_blank">
-                                    <div class ="form-child">
+                                    <div class="form-child">
                                         <label for="text">Tên khoản thu nhập</label>
-                                        <input type="text" name="full_name" >
+                                        <input type="text" name="full_name">
                                     </div>
-                                    <div class ="form-child">
+                                    <div class="form-child">
                                         <label for="text">Số tiền</label>
-                                        <input type="tel" name="phone_number" >
+                                        <input type="tel" name="phone_number">
                                     </div>
-                                    <div class ="form-child">
+                                    <div class="form-child">
                                         <label for="text">Ghi chú</label>
-                                        <input type="email" name="email" >
+                                        <input type="email" name="email">
                                     </div>
-                                    
-                                    <div class ="form-child">
+
+                                    <div class="form-child">
                                         <button type="submit">Cập Nhật</button>
                                     </div>
                                 </form>
-                              </div>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -141,60 +138,53 @@
 
         <!-- BANG CHI TIEU -->
         <table border="1">
-            <caption align="bottom">DANH SÁCH CHI TIÊU</caption>
             <tr>
-                <th>Tên khoản chi tiêu</th>
+                <th>Tên khoản thu nhập</th>
                 <th>Số tiền</th>
                 <th>Ghi chú</th>
                 <th>Ngày</th>
-                <th>Loại chi tiêu</th>
-                <th></th>
-                <th></th>
+                <th>Loại thu nhập</th>
+                <th>Thao tác</th>
             </tr>
             <tr>
-                <td>Bánh gà</td>
-                <td>20.000</td>
+                <td>Lương</td>
+                <td>20.000.000</td>
                 <td></td>
                 <td>1/6/2023</td>
-                <td>Đồ ăn</td>
-                <td><button  onclick="momodal2()"><span>Sửa</span></button></td>
-                <td><button  onclick="momodal3()"><span>Xóa</span></button></td>
+                <td>Thu nhập chính</td>
+                <td><a href="#" onclick="momodal2()" class="btn btn-success" name="suathunhap">Sửa</a>
+                    <a href="#" onclick="momodal3()" class="btn btn-danger" name="xoathunhap" onclick="confirm('Bạn có chắc chắn muốn xóa?')">Xóa</a>
+                </td>
             </tr>
             <tr>
-                <td>Quần áo bà ba</td>
-                <td>200.000</td>
-                <td>Tặng bà</td>
+                <td>Chứng khoán</td>
+                <td>10.000.000</td>
+                <td>Đầu tư</td>
                 <td>1/6/2023</td>
-                <td>Quần áo</td>
-                <td><button  onclick="momodal2()"><span>Sửa</span></button></td>
-                <td><button  onclick="momodal3()"><span>Xóa</span></button></td>
+                <td>Thu nhập thêm</td>
+                <td><a href="#" onclick="momodal2()" class="btn btn-success" name="suathunhap">Sửa</a>
+                    <a href="#" onclick="momodal3()" class="btn btn-danger" name="xoathunhap" onclick="confirm('Bạn có chắc chắn muốn xóa?')">Xóa</a>
+                </td>
             </tr>
             <tr>
-                <td>Tiền điện tháng 5</td>
-                <td>400.000</td>
+                <td>Kinh doanh</td>
+                <td>40.000.000</td>
                 <td></td>
                 <td>7/6/2023</td>
-                <td>Tiền điện</td>
-                <td><button  onclick="momodal2()"><span>Sửa</span></button></td>
-                <td><button  onclick="momodal3()"><span>Xóa</span></button></td>
+                <td>Thu nhập chính</td>
+                <td><a href="#" onclick="momodal2()" class="btn btn-success" name="suathunhap">Sửa</a>
+                    <a href="#" onclick="momodal3()" class="btn btn-danger" name="xoathunhap" onclick="confirm('Bạn có chắc chắn muốn xóa?')">Xóa</a>
+                </td>
             </tr>
             <tr>
-                <td>Mua trà sữa</td>
+                <td>Bán lại đồ cũ</td>
                 <td>30.000</td>
                 <td></td>
                 <td>7/6/2023</td>
-                <td>Đồ uống</td>
-                <td><button  onclick="momodal2()"><span>Sửa</span></button></td>
-                <td><button  onclick="momodal3()"><span>Xóa</span></button></td>
-            </tr>
-            <tr>
-                <td>Đi du lịch</td>
-                <td>1.500.000</td>
-                <td>Hạ Long</td>
-                <td>8/6/2023</td>
-                <td>Du lịch</td>
-                <td><button  onclick="momodal2()"><span>Sửa</span></button></td>
-                <td><button  onclick="momodal3()"><span>Xóa</span></button></td>
+                <td>Thu nhập thêm</td>
+                <td><a href="#" onclick="momodal2()" class="btn btn-success" name="suathunhap">Sửa</a>
+                    <a href="#" onclick="momodal3()" class="btn btn-danger" name="xoathunhap" onclick="confirm('Bạn có chắc chắn muốn xóa?')">Xóa</a>
+                </td>
             </tr>
             <tr>
                 <td>Nhét lợn</td>
@@ -202,8 +192,9 @@
                 <td></td>
                 <td>8/6/2023</td>
                 <td>Tiết kiệm</td>
-                <td><button  onclick="momodal2()"><span>Sửa</span></button></td>
-                <td><button  onclick="momodal3()"><span>Xóa</span></button></td>
+                <td><a href="#" onclick="momodal2()" class="btn btn-success" name="suathunhap">Sửa</a>
+                    <a href="#" onclick="momodal3()" class="btn btn-danger" name="xoathunhap" onclick="confirm('Bạn có chắc chắn muốn xóa?')">Xóa</a>
+                </td>
             </tr>
         </table>
     </div>
@@ -224,22 +215,23 @@
                     <p class="item-desc">Bản quyền thuộc về Nhóm 5 lớp IT6059-001</p>
                 </div>
                 <div class="item-right col-12 col-lg-4 col-md-4 col-sm-4">
-                    <img src="./assets/images/footer_logo.png" alt="Logo footer">
+                    <img src="../../assets/images/footer_logo.png" alt="Logo footer">
                 </div>
             </div>
         </div>
     </footer>
-    <script src="assets/bootstrap/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+    <script src="../../assets/bootstrap/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
     </script>
     <script>
-        function momodal(){
+        function momodal() {
             document.getElementById("nenmodal-1").classList.toggle("active");
         }
-        function momodal2(){
+
+        function momodal2() {
             document.getElementById("nenmodal-2").classList.toggle("active");
         }
-        function momodal3(){
+
+        function momodal3() {
             document.getElementById("nenmodal-3").classList.toggle("active");
         }
     </script>

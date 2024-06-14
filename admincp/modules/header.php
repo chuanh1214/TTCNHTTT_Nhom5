628b4997e629f5ee89625ce3de853a7c9c380d56
chuanh1214 <chuthianh2202chuthianh22@gmail.com>
2024-06-14 18:08:07 +0700

    no commit

--- admincp/modules/header.php
+++ admincp/modules/header.php
@@ -11,12 +11,12 @@ if (isset($_GET['dangxuat']) && $_GET['dangxuat'] == 1) {
                 <div class="inner-main">
                     <a class="inner-logo" href="index.php">
                         <img src="assets/images/moneyManager.png" alt="Logo moneyManager">
-                        <span class="inner-name">Money Manager</span>
+                        <span class="inner-name">Admin Manager</span>
                     </a>
                     <div class="inner-menu">
                         <ul>
-                            <li><a href="index.php?action=quanlyloaithunhap&query=lietke">Quản lý loại thu nhập</a></li>
-                            <li><a href="index.php?action=quanlyloaichitieu&query=lietke">Quản lý loại chi tiêu</a></li>
+                            <li><a href="index.php?action=quanlyloaithunhap&query=lietke">Loại thu nhập</a></li>
+                            <li><a href="index.php?action=quanlyloaichitieu&query=lietke">Loại chi tiêu</a></li>
                             <li><a href="index.php?dangxuat=1">Đăng xuất : <?php if (isset($_SESSION['dangnhap'])) {
                                                                                 echo $_SESSION['dangnhap'];
                                                                             } ?></a></li>
